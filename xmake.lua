32353e74a650dadd470e1a4ef9661e80d25bc469
Hato1125 <hato.flavius@gmail.com>
2026-02-13 18:57:18 +0900

    refactor: move source files into src directory

--- xmake.lua
+++ xmake.lua
@@ -1,11 +1,19 @@
 add_rules('mode.debug', 'mode.release')
 
-add_rules('plugin.compile_commands.autoupdate', {
-  outputdir = 'build'
-})
+option('compile_commands')
+  set_default(false)
+  set_showmenu(true)
+  set_description('Generate compile_commands.json')
+option_end()
 
+if has_config('compile_commands') then
+  ---@diagnostic disable-next-line: param-type-mismatch
+  add_rules('plugin.compile_commands.autoupdate', { outputdir = 'build' })
+end
+
+set_allowedplats('linux')
 set_languages('c++26')
 
 target('cramshell')
   set_kind('binary')
-  add_files('**.cc')
+  add_files('src/**.cc')
