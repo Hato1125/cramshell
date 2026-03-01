7e4981b2e3ec5b1e81c8bdfc00013cd7e2f280fa
Hato1125 <hato.flavius@gmail.com>
2026-03-01 23:06:09 +0900

    feat: creating an installation script with openrc

---
+++ service/openrc/install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if ! xmake; then
+  exit 1
+fi
+
+ARCH=$(uname -m)
+sudo cp build/linux/${ARCH}/release/clamshell /usr/bin/
+sudo cp service/openrc/clamshell.in /etc/init.d/clamshell
+sudo chmod +x /etc/init.d/clamshell
